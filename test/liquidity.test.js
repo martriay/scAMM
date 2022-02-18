2bb91330c82c2f0ea4d08811edd4d35be43bef0d
eugenioclrc <eugenioclrc@gmail.com>
2022-02-18 10:38:19 -0300

    fix liquidity test

--- test/liquidity.test.js
+++ test/liquidity.test.js
@@ -4,8 +4,6 @@ const { ethers, waffle } = require("hardhat");
 let token;
 let exchange;
 
-let tx;
-
 let deployer, bob;
 
 describe("Liquidity", function () {
@@ -72,50 +70,56 @@ describe("Liquidity", function () {
     }
   });
 
-  it("cant fuck liquidity seding raw ETH", async function () {
-    const tx = deployer.sendTransaction({
-      to: exchange.address,
-      value: ethers.utils.parseEther("0.01"),
-    });
-    
-    await expect(tx).to.reverted;
-  });
-
-  it("cant fuck liquidity seding raw FTH", async function () {
-    const amountA = ethers.utils.parseEther("0.01");
-    const amountB = ethers.utils.parseEther("10");
-    await token.approve(exchange.address, amountA.mul("10"));
-    let tx = exchange.addLiquidity(amountA, { value: amountB });
-    
-    // el orden del testeo es importante, al cambiarlo falla waffle :S
-    // await expect(() => tx)
-    //   .to.changeTokenBalances(token, [deployer, exchange], [amountA.mul("-1"), amountA]);
-  
-    await expect(await tx)
-      .to.changeEtherBalances([deployer, exchange], [amountB.mul("-1"), amountB])
-      .to.emit(token, "Transfer").withArgs(deployer.address, exchange.address, amountA)
-      .to.emit(exchange, "AddLiquidity").withArgs(deployer.address, amountB, amountA);
-    
+  describe('Fuck addLiquidity', function() {
+    it("cant fuck liquidity seding raw ETH", async function () {
+      const tx = deployer.sendTransaction({
+        to: exchange.address,
+        value: ethers.utils.parseEther("0.01"),
+      });
       
-    let ethReserve = await exchange.provider.getBalance(exchange.address);
-    let tokenReserve = await exchange.getReserve();
-    let tokenAmount = amountA.mul(tokenReserve).div(ethReserve);
-    
-    console.log("Ratio antes de enviar FTH", ethers.utils.formatEther(tokenAmount));
-    await token.transfer(exchange.address, amountB);
-          
-    ethReserve = await exchange.provider.getBalance(exchange.address);
-    tokenReserve = await exchange.getReserve();
-    tokenAmount = amountA.mul(tokenReserve).div(ethReserve);
+      await expect(tx).to.reverted;
+    });
 
-    console.log("Ratio despues de enviar FTH", ethers.utils.formatEther(tokenAmount));
+    it("cant fuck liquidity seding raw FTH", async function () {
+      const amountA = ethers.utils.parseEther("0.01");
+      const amountB = ethers.utils.parseEther("10");
+      // apruebo de mas para hacer el approve una sola vez
+      await token.approve(exchange.address, ethers.constants.MaxUint256);
 
-    // agrego liquidez usando exactamente la misma cantidad que tiene el LP en ETH y FTH
-    await expect(await tx)
-      .to.changeEtherBalances([deployer, exchange], [ethReserve.mul("-1"), ethReserve])
-      .to.emit(token, "Transfer").withArgs(deployer.address, exchange.address, tokenReserve)
-      .to.emit(exchange, "AddLiquidity").withArgs(deployer.address, ethReserve, tokenReserve);
-    
-    
+      // agrego liquidez una vez
+      let tx = exchange.addLiquidity(amountA, { value: amountB });
+      await expect(await tx)
+        .to.changeEtherBalances([deployer, exchange], [amountB.mul("-1"), amountB])
+        .to.emit(token, "Transfer").withArgs(deployer.address, exchange.address, amountA)
+        .to.emit(exchange, "AddLiquidity").withArgs(deployer.address, amountB, amountA);
+
+      // agrego liquidez por segunda vez con el mismo ratio
+      tx = exchange.addLiquidity(amountA, { value: amountB });
+      await expect(await tx)
+        .to.changeEtherBalances([deployer, exchange], [amountB.mul("-1"), amountB])
+        .to.emit(token, "Transfer").withArgs(deployer.address, exchange.address, amountA)
+        .to.emit(exchange, "AddLiquidity").withArgs(deployer.address, amountB, amountA);
+      
+      // agrego el token FTH al pool por transferencia, cambiando el ratio
+      await token.transfer(exchange.address, amountB.div("20"));
+
+      // agrego liquidez por tercera vez despues de joder el ratio, esto tiene que fallar
+      expect(exchange.addLiquidity(amountA, { value: amountB })).to.be.revertedWith("insufficient token amount");
+      
+      const tokenReserve = await exchange.getReserve();
+      const ethReserve = await exchange.provider.getBalance(exchange.address);
+
+      const newAmountA = tokenReserve.div("10");
+      const newAmountB = ethReserve.div("10");
+      
+      tx = exchange.addLiquidity(newAmountA, { value: newAmountB });
+      
+      // agrego liquidez usando exactamente la misma cantidad que tiene el LP en ETH y FTH
+      await expect(await tx)
+        .to.changeEtherBalances([deployer, exchange], [newAmountB.mul("-1"), newAmountB])
+        .to.emit(token, "Transfer").withArgs(deployer.address, exchange.address, newAmountA)
+        .to.emit(exchange, "AddLiquidity").withArgs(deployer.address, newAmountB, newAmountA);
+
+    });
   });
 });
